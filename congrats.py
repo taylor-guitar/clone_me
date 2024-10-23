26b375da1a8bc07795964664cc5a7860ddc88048
valentin-datacraft <valentin@data-craft.de>
2024-10-23 18:33:03 +0200

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -5,3 +5,4 @@ now = datetime.now().replace(microsecond=0)
 print('Hello student! you have cloned your first repository.')
 print(f'Note this historic moment: {now}.')
 print("Congratulations! Now it's time to produce repositories of you own!")
+# This should go into abwege branch
