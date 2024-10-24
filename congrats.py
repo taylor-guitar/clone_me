d68dc3e389c23f22d50dd5d78ccd44c6bf60c39f
alkankerim <kerim.alkan@data-craft-students.de>
2024-10-24 11:59:36 +0200

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+from tornado.http1connection import parse_int
+
 now = datetime.now().replace(microsecond=0)
 
 print('Hello student! you have cloned your first repository.')
 print(f'Note this historic moment: {now}.')
 print("Congratulations! Now it's time to produce repositories of your own!")
+print('ich war in dein code :)')
