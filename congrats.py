6f84a6c3df28cbd8761d5857d74e354ce990277a
alkankerim <kerim.alkan@data-craft-students.de>
2024-10-24 12:11:16 +0200

    Update congrats.py

--- congrats.py
+++ congrats.py
@@ -7,4 +7,7 @@ now = datetime.now().replace(microsecond=0)
 print('Hello student! you have cloned your first repository.')
 print(f'Note this historic moment: {now}.')
 print("Congratulations! Now it's time to produce repositories of your own!")
+<<<<<<< Updated upstream
 print('ich war in deinem code :)')
+=======
+>>>>>>> Stashed changes
